0833ed56b035fff7d2e2681fcff7d25c8b8ff036
Kunwoo Ahn <dryrain@fastmail.com>
2022-05-19 10:52:45 +0900

    .

--- Parts/DGFoodConvert.py
+++ Parts/DGFoodConvert.py
@@ -7,9 +7,16 @@ class DGFoodConvert:
     def __init__(self, dg_food_fetch):
         self.dg_food_fetch = dg_food_fetch
 
-    def convert_to_csv(self) -> List[List[str]]:
+    def convert_to_csv(self, x) -> List[List[str]]:
         dg_food_fetch: List[dict] = self.dg_food_fetch
-        ...
+        dgfoodcnt = x
+        chart=[[] for x in range(dgfoodcnt)]
+        for i in range(0, len(dg_food_fetch)):
+            chart[i].append(dg_food_fetch[i].values())
+         #   for a in dg_food_fetch[i].keys(): # key리스트를 만들어 a에 옮긴 후
+         #       chart[i].append(dg_food_fetch[i][a].split()) # fetch 파일에서 키값을 제외하고 저장
+        return chart
+        ... 
 
     ...
 
@@ -33,10 +40,41 @@ if __name__ == '__main__':
             "OPENDATA_ID": "1234",
             "GNG_CS": "대구광역시 북구 구암동 ****",
             "FD_CS": "중화요리",
+        },
+        {
+            "cnt": "3",
+            "OPENDATA_ID": "4567",
+            "GNG_CS": "대구광역시 북구 대학로 ****",
+            "FD_CS": "일식",
+        },
+        {
+            "cnt": "4",
+            "OPENDATA_ID": "7789",
+            "GNG_CS": "대구광역시 북구 고성로 ****",
+            "FD_CS": "카페",
+        },
+        {
+            "cnt": "5",
+            "OPENDATA_ID": "7789",
+            "GNG_CS": "대구광역시 북구 고성로 ****",
+            "FD_CS": "카페",
+        },
+        {
+            "cnt": "6",
+            "OPENDATA_ID": "7789",
+            "GNG_CS": "대구광역시 북구 고성로 ****",
+            "FD_CS": "카페",
         }
     ]
 
-    print("return: ", DGFoodConvert(_example_fetch).convert_to_csv())
+    cnt = len(_example_fetch)
+
+    dgFood = [[] for x in range(cnt)]
+
+    dgFood = DGFoodConvert(_example_fetch).convert_to_csv(cnt)
+
+    for i in range(0, len(dgFood)):
+        print("return: ", dgFood[i])
     # 예상 출력:
     # [
     #   [
