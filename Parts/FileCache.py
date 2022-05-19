383ca8b7da8e2c17309a9c84161b9c97f9487e4d
Kunwoo Ahn <dryrain@fastmail.com>
2022-05-19 11:31:25 +0900

    file cahce 에 로깅 추가

--- Parts/FileCache.py
+++ Parts/FileCache.py
@@ -1,4 +1,5 @@
 import diskcache
+import logging
 from diskcache import FanoutCache
 
 
@@ -21,30 +22,40 @@ class FileCache:
         if key is None:
             key = self.key
 
-        return self.cache.set(key=self.key_name(key), value=dict_data, expire=expire)
+        set_result = self.cache.set(key=self.key_name(key), value=dict_data, expire=expire)
+        logging.info(f"File Cache set: key={key} expire={expire} dict_data={dict_data}")
+        return set_result
 
     def get(self, default=None, key=None):
         if key is None:
             key = self.key
 
-        return self.cache.get(key=self.key_name(key), default=default)
+        get_result = self.cache.get(key=self.key_name(key), default=default)
+        logging.info(f"File Cache get: default={default} key={key} get_result={get_result}")
+        return get_result
 
     def rm(self, key=None):
         if key is None:
             key = self.key
 
-        return self.cache.delete(key=self.key_name(key))
+        del_result = self.cache.delete(key=self.key_name(key))
+        logging.info(f"File Cache rm: key={key} del_result={del_result}")
+        return del_result
 
     def get_or_call(self, fun, args: tuple, expire=None, key=None):
         if key is None:
             key = self.key
 
+        cache_hit = True
         data = self.get(default=None)
 
         if data is None:
+            cache_hit = False
             data = fun(*args)
             self.set(data, expire=expire, key=key)
 
+        logging.info(f"File Cache get_or_call: "
+                     f"fun={fun.__name__} args={args} key={key} expire={expire} cache_hit={cache_hit} data={data}")
         return data
 
 
